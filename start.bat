767572d608f34979157364ea063b9918d960747a
Dylan Turner <dylan.turner@tutanota.com>
2024-03-13 17:51:09 -0500

    start: moved to run to make it background
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- start.bat
+++ start.bat
@@ -1,9 +1,3 @@
 @echo off
-
-REM Set up environment. Does nothing if already installed
-python -m virtualenv .venv
-.\.venv\Scripts\pip install pyvda pystray
-
-REM Actually run the script
-.\.venv\Scripts\python wws.py
+start /min cmd.exe /c run.bat
 
