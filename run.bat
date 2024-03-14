e46e43cdfd73a0fbcf4c2eebcb61c4c646fcc7c9
Dylan Turner <dylan.turner@tutanota.com>
2024-03-14 10:09:38 -0500

    wws: version-locked dependencies
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 
 REM Set up environment. Does nothing if already installed
 python -m virtualenv .venv
-.\.venv\Scripts\pip install pyvda pystray
+.\.venv\Scripts\pip install pyvda==0.4.3 pystray==0.19.5
 
 REM Actually run the script
 .\.venv\Scripts\python wws.py
