978684a6778fbfe71aae8b4782ed7d24fd305504
Dylan Turner <dylan.turner@tutanota.com>
2024-03-14 14:46:58 -0500

    wws: "reload" option to fix incorrect order
    Signed-off-by: Dylan Turner <dylan.turner@tutanota.com>

--- wws.py
+++ wws.py
@@ -10,20 +10,30 @@ import time
 # ---------- Icon Stuff ----------
 
 def on_change_desktop_clicked(icon, item):
-    VirtualDesktop(number = int(icon.name)).go()
+    VirtualDesktop(number = int(icon.name.split('.')[0])).go()
 
+# Close all icons, but shouldn't directly handle other icons, so just set a global variable
 def on_exit_clicked(icon, item):
     global SHOULD_CLOSE
 
     SHOULD_CLOSE = True
 
+# Reposition all icons, but shouldn't directly handle other icons, so just set a global variable
+def on_reload_clicked(icon, item):
+    global RELOAD
+
+    RELOAD = True
+
 def icon_create(i, img_desel):
+    global THREAD_START_ID
+
     VirtualDesktop(number = i + 1).rename(f'Desktop {i + 1}')
     icon = Icon(
-        str(i + 1),
+        str(i + 1) + '.' + str(THREAD_START_ID),
         icon = img_desel,
         menu = Menu(
             MenuItem('ChangeDesktop', on_change_desktop_clicked, default = True),
+            MenuItem('Reload', on_reload_clicked),
             MenuItem('Exit', on_exit_clicked)
         )
     )
@@ -63,6 +73,8 @@ def desktops_make_enough(desktops):
 # ---------- Main ----------
 
 SHOULD_CLOSE = False
+RELOAD = False
+THREAD_START_ID = 0
 STARTUP_DELAY = 1.0
 NUM_DESKTOPS = 9        # Must be less than or equal to 10 bc I only have 10 pictures :)
 
@@ -81,10 +93,29 @@ def load_images():
     desel_imgs.pop(0)
     return (sel_imgs, desel_imgs)
 
+def spawn_icons(desel_imgs):
+    global THREAD_START_ID
+
+    icons = [ icon_create(i, desel_imgs[i]) for i in range(NUM_DESKTOPS) ]
+    threads = []
+    spawn_order = list(range(NUM_DESKTOPS)[-1:0:-1])
+    spawn_order.insert(0, 0)
+    for i in spawn_order: # They don't show up in the correct order otherwise
+        thread = Thread(
+            target = icons[i].run,
+            name = f'VirtDeskSystrayIcon_{i + 1}.{THREAD_START_ID}'
+        )
+        threads.append(thread)
+        thread.start()
+        time.sleep(STARTUP_DELAY) # Also needed for delay
+    THREAD_START_ID += 1
+    return (icons, threads)
+
 def main():
     global SHOULD_CLOSE
     global STARTUP_DELAY
     global NUM_DESKTOPS
+    global RELOAD
 
     (sel_imgs, desel_imgs) = load_images()
     
@@ -92,26 +123,29 @@ def main():
     desktops = desktops_remove_extra(desktops)
     desktops = desktops_make_enough(desktops)
 
-    icons = [ icon_create(i, desel_imgs[i]) for i in range(NUM_DESKTOPS) ]
-    threads = []
-    spawn_order = list(range(NUM_DESKTOPS)[-1:0:-1])
-    spawn_order.insert(0, 0)
-    for i in spawn_order: # They don't show up in the correct order otherwise
-        thread = Thread(target = icons[i].run)
-        threads.append(thread)
-        thread.start()
-        time.sleep(STARTUP_DELAY) # Also needed for delay
+    (icons, threads) = spawn_icons(desel_imgs)
 
     last_cur_num = -1
     while not SHOULD_CLOSE:
+        # Make the current desktop be highlighted
         cur_num = VirtualDesktop.current().number
         if last_cur_num != cur_num:
             for icon in icons:
-                if icon.name == str(cur_num):
+                if icon.name.split('.')[0] == str(cur_num):
                     icon.icon = sel_imgs[cur_num - 1]
                 else:
-                    icon.icon = desel_imgs[int(icon.name) - 1]
+                    icon.icon = desel_imgs[int(icon.name.split('.')[0]) - 1]
             last_cur_num = cur_num
+
+        # Sometimes icons get out of order. This respawns them
+        if RELOAD:
+            for icon in icons:
+                icon.stop()
+            for thread in threads:
+                thread.join()
+            (icons, threads) = spawn_icons(desel_imgs)
+            RELOAD = False
+
         time.sleep(0.01)
 
     for icon in icons:
